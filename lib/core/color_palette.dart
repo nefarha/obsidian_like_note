fda194a77b0a368bc3449695dfbf2fca06da046b
Farhan Febryan <nefarha1@gmail.com>
2024-12-12 22:06:35 +0700

    added core files

---
+++ lib/core/color_palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const nightBlack = Color(0xff2D3250);
+  static const nightGrey = Color(0xffD8D9DA);
+}
---
+++ lib/core/common_utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:obsidian_like_note/core/color_palette.dart';
+
+class CommonUtils {
+  static TextStyle headerStyle = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: ColorPalette.nightGrey,
+  );
+}
